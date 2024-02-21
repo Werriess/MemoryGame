e04e5a57a3be1620117f9c0075fa07eef5b60932
Werner Janse van Rensburg <wernerjvanrensburg15@gmail.com>
2024-02-21 22:14:47 +0200

    Add basic design of website

---
+++ public/index.js
@@ -0,0 +1,7 @@
+console.log("Js is connected")
+const startGame = document.querySelector("#startGame");
+const orangeOne = document.querySelector("#orangeOne");
+
+startGame.addEventListener('click', () => {
+    orangeOne.
+})
